fe8a0ecfde33feec5940df47f6a4f89b0459e009
ashfaq <ashfaq@example.com>
2023-02-06 21:15:01 +0530

    Front End and back End set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/Homescreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
+    <BrowserRouter>
+
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </main>
+
+    </BrowserRouter>
   );
 }
 

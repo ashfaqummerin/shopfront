4c6563c566ac4e375694f92825e22ba6726a3064
ashfaq <ashfaq@example.com>
2023-02-15 16:46:31 +0530

    Back end user authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,12 @@
 import express from "express"
+import { getProductById, getProducts } from "../controllers/productController.js"
+
 const router = express.Router()
-import Product from "../models/productsModel.js"
-import asyncHandler from "express-async-handler"
 
-// @desc Fetch all products
-// @route GET /api/products
-// @acess public
 
-router.get("/", asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
+router.route("/").get(getProducts)
 
-// @desc Fetch single product
-// // @route GET /api/products/:id
-// // @acess public
-
-router.get("/:id", asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error("product not found")
-    }
-
-}))
+router.get("/:id", getProductById)
 
 
 export default router
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { authUser, getUserprofile, registerUser } from "../controllers/userController.js"
+import { protect } from "../middleware/authMiddleware.js"
+const router = express.Router()
+
+router.post("/", registerUser)
+router.post("/login", authUser)
+router.get("/profile", protect, getUserprofile)
+
+
+export default router

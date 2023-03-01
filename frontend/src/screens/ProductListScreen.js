49f54276e0fa02aa6a94efae5cbd7d727c721cc8
ashfaq <ashfaq@example.com>
2023-03-01 22:02:39 +0530

    Admin product list

---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,105 @@
+import { useDispatch, useSelector } from "react-redux"
+import axios from "axios";
+import { Button, Table, Row, Col } from "react-bootstrap"
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { LinkContainer } from "react-router-bootstrap"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { PRODUCT_DETAIL_FAIL, PRODUCT_DETAIL_REQUEST, PRODUCT_DETAIL_SUCCESS } from "../redux/productSlice";
+
+const ProductListScreen = () => {
+    const { id } = useParams()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    // PRODUCTS LIST STATE FROM STORE
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+
+    // LIST PRODUCTS ACTION
+
+    const listProducts = async () => {
+        try {
+            dispatch(PRODUCT_DETAIL_REQUEST())
+            const { data } = await axios.get(`/api/products`)
+            dispatch(PRODUCT_DETAIL_SUCCESS(data))
+        } catch (error) {
+            dispatch(PRODUCT_DETAIL_FAIL(error))
+        }
+    }
+
+    useEffect(() => {
+        if (userInfo && userInfo.isAdmin) {
+            listProducts()
+        } else {
+            navigate("/login")
+        }
+    }, [userInfo])
+
+    const deleteHandler = (id) => {
+        if ((window.confirm("Are you sure"))) {
+            // delete products
+        }
+    }
+
+    const createProductHandler = () => {
+        console.log("create")
+    }
+
+    return (
+        <>
+            <Row className="align-items-center">
+                <Col>
+                    <h1>Products</h1>
+                </Col>
+                <Col className="text-right">
+                    <Button className="my-3" onClick={createProductHandler}>
+                        <i className="fas fa-plus" /> Create Product
+                    </Button>
+                </Col>
+            </Row>
+
+            {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
+                <Table striped bordered hover responsive className="table-sm" >
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NAME</th>
+                            <th>PRICE</th>
+                            <th>CATEGORY</th>
+                            <th>BRAND</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map(product => (
+                            <tr key={product._id}>
+                                <td>{product._id}</td>
+                                <td>{product.name}</td>
+                                <td>$ {product.price}</td>
+                                <td>{product.category}</td>
+                                <td>{product.brand}</td>
+                                <td>
+                                    <LinkContainer to={`/admin/user/${product._id}/edit`}>
+                                        <Button variant="light" className="btn-sm">
+                                            <i className="fas fa-edit"></i>
+                                        </Button>
+                                    </LinkContainer>
+                                    <Button variant="danger" className="btn-sm" onClick={() => deleteHandler(product._id)}>
+                                        <i className="fas fa-trash"></i>
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
+        </>);
+}
+
+export default ProductListScreen;

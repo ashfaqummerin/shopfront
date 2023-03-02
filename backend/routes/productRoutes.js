f76d4ea29c623f8a5bc54113e2de14e4fde35f40
ashfaq <ashfaq@example.com>
2023-03-02 12:27:16 +0530

    Delete product from admin panel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,21 @@
 import express from "express"
-import { getProductById, getProducts, deleteProduct } from "../controllers/productController.js"
+import { getProductById, getProducts, deleteProduct, createProduct, updateProduct } from "../controllers/productController.js"
 import { protect, admin } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 
-router.route("/").get(getProducts)
+router
+    .route("/")
+    .get(getProducts)
+    .post(protect, admin, createProduct)
 
 router
     .route("/:id")
     .get(getProductById)
     .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct)
+
 
 
 export default router

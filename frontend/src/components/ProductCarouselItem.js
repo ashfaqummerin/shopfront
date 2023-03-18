412cda7ab1b7c9e312d41d989fa9155f5e1d29b3
ashfaq_ummer <ashfaqummerin@gmail.com>
2023-03-18 22:04:25 +0530

    Debugging carousel link

--- frontend/src/components/ProductCarouselItem.js
+++ frontend/src/components/ProductCarouselItem.js
@@ -2,14 +2,19 @@ import { Link } from "react-router-dom";
 import { Image } from "react-bootstrap";
 
 const ProductCarouselItem = ({ product }) => {
-    return (
-        <Link to={`/api/products/${product._id}`}>
-            <Image src={product.image} alt={product.name} className="carousel__image" />
-            <div className="carousel__text__container">
-                <h3 className="carousel__text">{product.name}</h3>
-                <h3 className="carousel__text">$ {product.price}</h3>
-            </div>
-        </Link>)
-}
+  return (
+    <Link to={`/product/${product._id}`}>
+      <Image
+        src={product.image}
+        alt={product.name}
+        className="carousel__image"
+      />
+      <div className="carousel__text__container">
+        <h3 className="carousel__text">{product.name}</h3>
+        <h3 className="carousel__text">$ {product.price}</h3>
+      </div>
+    </Link>
+  );
+};
 
 export default ProductCarouselItem;
